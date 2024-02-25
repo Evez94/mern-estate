ae17b504a5040549e79363161e441e201ce33598
Evez Aslanov <evez.aslanov.94@gmail.com>
2024-02-25 19:40:30 +0400

    backend routes,controllers,error handling, midlware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -10,8 +13,23 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000!!!");
 });
 
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+//midleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
