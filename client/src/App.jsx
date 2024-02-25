5c2d3fcdff5bcbdc452474344cfac3671e12e42d
Evez Aslanov <evez.aslanov.94@gmail.com>
2024-02-25 06:04:55 +0400

    creating pages, header component and adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
-export default function
-  () {
+
+export default function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
 
-    </div>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
